44ef0c7a7c193fbc0e5b22860bb9977c8d6dbc8a
stevensmithmsc <stevensmithmsc@gmail.com>
2016-03-02 16:21:47 +0000

    Inital Repo Created
    Inital Repository created, simple entity model created for app.

---
+++ MigrationMappings/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MigrationMappings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            MappingsDBContext db = new MappingsDBContext();
+            db.Maps.Load();
+            dataGridView1.DataSource = db.Maps.Local.ToBindingList();
+        }
+    }
+}
