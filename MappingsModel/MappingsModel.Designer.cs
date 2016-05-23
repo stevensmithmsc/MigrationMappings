1861b5889eda8f81e39d73717d1cf49769f1521b
Steven Smith <steven.smith5@nhs.net>
2016-05-23 12:58:59 +0100

    Seperate Model in Seperate Project

--- MappingsModel/MappingsModel.Designer.cs
+++ MappingsModel/MappingsModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Steve\Documents\GitHub\MigrationMappings\MigrationMappings\MappingsModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\steven.smith\VSProjects\Mappings2\MigrationMappings\MigrationMappings\MappingsModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
