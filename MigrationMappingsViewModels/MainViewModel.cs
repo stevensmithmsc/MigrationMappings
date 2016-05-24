21572316d92234e8f45c214fad2ec517dd25e6ff
Steven Smith <steven.smith5@nhs.net>
2016-05-24 20:03:41 +0100

    Add WPF Application
    Attempted to add WPF Application

---
+++ MigrationMappingsViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MigrationMappings;
+using System.Collections.ObjectModel;
+
+namespace MigrationMappingsViewModels
+{
+    public class MainViewModel
+    {
+        MappingsDBContext db;
+        ObservableCollection<RefArea> Areas { get; set; }
+
+        public MainViewModel()
+        {
+            db = new MappingsDBContext();
+            db.RefAreas.Load();
+            Areas = db.RefAreas.Local;
+        }
+    }
+}
