a716fd5fd8e9c17157c6ad83cea1a1b919742203
stevensmithmsc <stevensmithmsc@gmail.com>
2016-05-25 12:11:31 +0100

    Recreated App as wpf application

---
+++ MigrationMappingsApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MigrationMappingsApp.Entity;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+
+namespace MigrationMappingsApp.ViewModels
+{
+    class MainViewModel
+    {
+        MigrationEntities db;
+
+        public ObservableCollection<RefArea> Areas { get; set; }
+        public RefArea SelectedArea { get; set; }
+
+        public MainViewModel()
+        {
+            db = new MigrationEntities();
+            db.RefAreas.Load();
+            Areas = db.RefAreas.Local;
+            SelectedArea = Areas.First();
+        }
+    }
+}
