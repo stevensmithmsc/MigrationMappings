43268eef65ad7bfa23eefb22c1f80213e0db4b2e
stevensmithmsc <stevensmithmsc@gmail.com>
2016-05-27 16:51:52 +0100

    Mapping and Reference Values now editable
    Tables for Mappings and reference values now allow in memory data to be
    amended.

--- MigrationMappingsApp/ViewModels/MainViewModel.cs
+++ MigrationMappingsApp/ViewModels/MainViewModel.cs
@@ -6,22 +6,52 @@ using System.Threading.Tasks;
 using MigrationMappingsApp.Entity;
 using System.Collections.ObjectModel;
 using System.Data.Entity;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
-namespace MigrationMappingsApp.ViewModels
+namespace MigrationMappingsApp.ViewModels 
 {
-    class MainViewModel
+    class MainViewModel : INotifyPropertyChanged
     {
         MigrationEntities db;
 
         public ObservableCollection<RefArea> Areas { get; set; }
-        public RefArea SelectedArea { get; set; }
+        private RefArea _SelectedArea;
+        public RefArea SelectedArea { get { return _SelectedArea; } set { _SelectedArea = value; FilterMapsandValues(); NotifyPropertyChanged();  } }
+        public List<Map> Mappings { get; set; }
+        public List<RefVal> Values { get; set; }
+
 
         public MainViewModel()
         {
             db = new MigrationEntities();
-            db.RefAreas.Load();
+            db.RefAreas.OrderBy(a => a.Area).Load();
             Areas = db.RefAreas.Local;
-            SelectedArea = Areas.First();
+            SelectedArea = Areas.OrderBy(a => a.ID).First();
+            
         }
+
+        private void FilterMapsandValues()
+        {
+            db.Maps.Where(m => m.Area == SelectedArea.ID).Load();
+            Mappings = db.Maps.Local.Where(m => m.Area == SelectedArea.ID).OrderBy(m => m.Local_Description).ToList();
+            NotifyPropertyChanged("Mappings");
+            db.RefVals.Where(v => v.Area == SelectedArea.ID).Load();
+            Values = db.RefVals.Where(v => v.Area == SelectedArea.ID).OrderBy(v => v.Description).ToList();
+            NotifyPropertyChanged("Values");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public ICommand LoadLogFileCommand { get; private set; }
     }
 }
